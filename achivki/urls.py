3f25758ec6f447f0805334f07dcf285b7ce0167b
Deceiveless <ann1137@ya.ru>
2012-02-18 14:42:21 +0400

    Links added in stub.html.

--- achivki/urls.py
+++ achivki/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,7 +10,6 @@ urlpatterns = patterns('',
     # url(r'^achivki/', include('achivki.foo.urls')),
 
     url(r'^$', 'django.contrib.staticfiles.views.serve', {'path': 'stub.html'}),
-
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-) 
+)
