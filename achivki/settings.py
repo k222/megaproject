218070ff568593d4222927c8b454a5ded0a041a4
esgv <ecxsgv@gmail.com>
2012-03-03 14:20:31 +0400

    Added admin static files. Added pinky pie pic.

--- achivki/settings.py
+++ achivki/settings.py
@@ -72,7 +72,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_ROOT + 'achivki/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
